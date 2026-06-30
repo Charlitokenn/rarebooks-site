ea0088af21db4f8d8bd250672b11d1b6f2322a33
Charles Nkonoki <charlitokenn@gmail.com>
2026-06-30 11:23:14 +0300

    fix: updated prop

--- src/components/components/nav-user.tsx
+++ src/components/components/nav-user.tsx
@@ -38,12 +38,12 @@ export function NavUser({ userDetails }: { userDetails: any }) {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={userDetails.imageUrl} alt={userDetails.id} />
-                <AvatarFallback className="rounded-lg">{GetInitials(userDetails.firstName, userDetails.lastName)}</AvatarFallback>
+                <AvatarImage src={userDetails.image_Url} alt={userDetails.id} />
+                <AvatarFallback className="rounded-lg">{GetInitials(userDetails.first_name, userDetails.last_name)}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">
-                  {userDetails.firstName} {userDetails.lastName}
+                  {userDetails.full_name}
                 </span>
                 <span className="truncate text-xs">
                   {userDetails.emailAddresses[0].emailAddress}
@@ -62,18 +62,6 @@ export function NavUser({ userDetails }: { userDetails: any }) {
             align="end"
             sideOffset={4}
           >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={userDetails.imageUrl} alt={userDetails.firstName} />
-                  <AvatarFallback className="rounded-lg">{GetInitials(userDetails.firstName, userDetails.lastName)}</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{userDetails.firstName} {userDetails.lastName}</span>
-                  <span className="truncate text-xs">{userDetails.emailAddresses[0]}</span>
-                </div>
-              </div>
-            </DropdownMenuLabel>
             <DropdownMenuItem onClick={() => console.log("signed out")}>
                 <HugeiconsIcon icon={LogoutIcon} strokeWidth={2} />
                 Log out
