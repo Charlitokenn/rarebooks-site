f63e6904f4aae7b46612a85ee2207ff6bf20c6d4
Charles Nkonoki <charlitokenn@gmail.com>
2026-06-30 12:34:41 +0300

    fix: moved logo back to assets folder and corrected logo path

--- astro.config.mjs
+++ astro.config.mjs
@@ -85,7 +85,7 @@ export default defineConfig({
         },
       ],
       logo: {
-        src: "/logo.png",
+        src: "./src/assets/logo.png",
         replacesTitle: false,
       },
       customCss: [
