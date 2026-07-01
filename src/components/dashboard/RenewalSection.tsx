d1ae302de3d69166fa4715329ed1e9206fd32062
Charles Nkonoki <charlitokenn@gmail.com>
2026-07-01 23:29:24 +0300

    fixed types errors due to missing shadcn components

--- src/components/dashboard/RenewalSection.tsx
+++ src/components/dashboard/RenewalSection.tsx
@@ -1,4 +1,4 @@
-import * as React from "import * as React from "react";
+import * as React from "react";
 import { useState, useEffect } from "react";
 import { MapPin } from "lucide-react";
 import { Card, CardContent } from "@components/components/ui/card";
